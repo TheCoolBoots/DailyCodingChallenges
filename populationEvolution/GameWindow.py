739a58367fb3be6bb1675d065e8c15b09b245432
Andrew Lai <arjlai221@gmail.com>
2020-10-29 13:46:56 -0700

    introducing graphics and game window

---
+++ populationEvolution/GameWindow.py
@@ -0,0 +1,10 @@
+from graphics import *
+import tkinter as tk
+
+class GameWindow:
+    
+    def __init__(self):
+        displayArea = GraphWin('Evolution is real', 300, 300)
+    
+if __name__ == "__main__":
+    test = GameWindow()
